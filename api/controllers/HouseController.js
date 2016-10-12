b48d0dd2ceed7ba5a8cec17affc678304b599954
sxm0617 <12251216@life.hkbu.edu.hk>
2016-10-13 00:11:07 +0800

    Improve the search function and add paginate function. Some problems occur and remain to be settled

--- api/controllers/HouseController.js
+++ api/controllers/HouseController.js
@@ -8,14 +8,72 @@
 module.exports = {
 	search: function(req, res) {
 		if (req.method == "GET") {
+			console.log("get");
 			User.findOne({username: req.session.username})
 				.exec(function(err, user) {
 					if (user == null) {
 						return res.redirect('/user/login');
 					} else {
-						return res.view('search');
+						House.find().where({district: ""}).exec(function(err, houses) {
+							return res.view('search', {'house': houses, 'count': 1, 'current': 1});				 					
+						});
 					}
-				});			
+				});	
+		} else {
+			// console.log("here");			
+			// console.log("here2");
+			// House.find()	
+			// 	.where({district: {contains: req.query.district}})
+			// 	.where({bedroom: {contains: req.query.bedroom}})
+			// 	.where({lift: {contains: req.query.lift}})
+			// 	.where({guard: {contains: req.query.guard}})
+			// 	.sort('id')
+			// 	.paginate({page: req.query.page, limit: 2})	
+			// 	.exec(function(err, houses) {
+			// 		console.log("here3")
+			// 		House.count({district: {contains: req.query.district}}, {bedroom: {contains: req.query.bedroom}},
+			// 				{lift: {contains: req.query.lift}}, {guard: {contains: req.query.guard}})
+			// 			.exec(function(err, value) {
+			// 				var pages = Math.ceil(value / 2	);
+			// 				return res.view('search', {'house': houses, 'count': pages, 'current': req.query.page});
+			// 		})					 					
+			// 	});
+		}
+	},
+
+	result: function(req, res) {
+		if (req.method == "GET") {
+			var houseModel = {
+				district: req.query.district,
+				bedroom: req.query.bedroom,
+				lift: req.query.lift,
+				guard:req.query.guard
+			};
+			if (houseModel.district == "") {
+				delete houseModel.district;
+			}
+			if (houseModel.bedroom == "") {
+				delete houseModel.bedroom;
+			}
+			if (houseModel.lift == "") {
+				delete houseModel.lift;
+			}
+			if (houseModel.district == "") {
+				delete houseModel.guard;
+			}
+			
+			House.find(houseModel)
+				.sort('id')
+				.paginate({page: req.query.page, limit: 2})
+				.exec(function(err, houses) {
+					House.count({district: {contains: req.query.district}}, {bedroom: {contains: req.query.bedroom}},
+							{lift: {contains: req.query.lift}}, {guard: {contains: req.query.guard}})
+						.exec(function(err, value) {
+							var pages = Math.ceil(value / 2	);
+							return res.view('search', {'house': houses, 'count': pages, 'current': req.query.page});
+					})
+				});
+			
 		}
 	},
 
